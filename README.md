52816611863002bf85c10330eda5e384e2d643bf
Jacob Kriese <jkriese12@gmail.com>
2020-12-08 21:31:18 -0700

    changed functions to use innerHTML changes to significantly consolidate script and limit use of jqueryin the code, completed readMe with pictures and links, no headway on local storage

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Java-Quiz
+# JavaScript-Quiz
+
+Multiple choice 6 question quiz built using HTML, CSS and JavaScript - Utilizing some Jquery as well in the code. User is presented with their instructions for the quiz on the main page and are taken
+to the quiz upon clicking the link. The quiz is timed for 60 seconds and the user is docked 10 seconds off the clock each time they answer a question incorrectly. If they do not complete the quiz in time they are taken to the 'Try again' page which will prompt the user to retake the quiz. If they are able to complete the quiz it will indicate to the user how many answers they got correct as well as how much time they had remaining. There is also a link to submit their highscore as well.
+
+Upcoming updates: Getting highscores to generate and be able to save to local storage.
+
+Please see pictures below of different pages of the webpage
+Main
+![image](https://user-images.githubusercontent.com/73569538/101584896-dc14d900-399b-11eb-823c-49ac1164b2cf.png)
+Quiz
+![image](https://user-images.githubusercontent.com/73569538/101584965-06ff2d00-399c-11eb-87a7-9e4516dba54e.png)
+Times-up
+![image](https://user-images.githubusercontent.com/73569538/101584995-1c745700-399c-11eb-9dcf-fdfe4904cfb2.png)
+Score display
+![image](https://user-images.githubusercontent.com/73569538/101585054-39a92580-399c-11eb-9212-882c7c5c6588.png)
+Scorebaord
+![image](https://user-images.githubusercontent.com/73569538/101585086-4cbbf580-399c-11eb-8c58-62099dc4ebc3.png)
+
+Link to github page: https://jkriese12.github.io/Java-Quiz/
+
+Link to github repo: https://github.com/jkriese12/Java-Quiz
--- script.js
+++ script.js
@@ -1,304 +1,311 @@
-    // Timer for quiz
-    var seconds = document.getElementById("countdown").textContent;
-    var countdown = setInterval(function() {
-        seconds--;
-        document.getElementById("countdown").textContent = seconds;
-        if (seconds <= 0) {clearInterval(countdown); alert("TIMES UP"); window.location="try-again.html"; } 
-    }, 1000); 
-    
-    //Setting variable selectors
-    var choice1 = document.getElementById("choice1");
-    var choice2 = document.getElementById("choice2");
-    var choice3 = document.getElementById("choice3");
-    var choice4 = document.getElementById("choice4");
-    var questionText = document.querySelector(".questionText");
-    var answerCorrect = 0;
-   
-    // Question one content
-    var questionOne = "Inside which HTML element do we put the JavaScript?"
-    var q1c1 = "<script>";
-    var q1c2 = "<scripting>";
-    var q1c3 = "<javascript>";
-    var q1c4 = "<js>";
-   
-    // Question two content
-    var questionTwo = "How do you write ''Hello World'' in an alert box?";
-    var q2c1 = "msgBox(''Hello World'');";
-    var q2c2 = "msg(''Hello World'');";
-    var q2c3 = "alertBox(''Hello World'');";
-    var q2c4 = "alert(''Hello World'');";
-   
-    // Question three content
-    var questionThree = "How do you write an IF statement in JavaScript";
-    var q3c1 = "if i = 5";
-    var q3c2 = "if i == 5 then";
-    var q3c3 = "if i = 5 then";
-    var q3c4 = "if (i == 5)";
-   
-    // Question four content
-    var questionFour = "How does a FOR loop start?";
-    var q4c1 = "for (i = 0; i < 5; i++)";
-    var q4c2 = "for (i = 0; i < 5)";
-    var q4c3 = "for (i < 5; i++)";
-    var q4c4 = "for (i =1 to 5)";
-   
-    // Question five content
-    var questionFive = "What is the correct way to write a JavaScript array?";
-    var q5c1 = "var colors = [“red”, “green”, “blue”]";
-    var q5c2 = "var colors = “red”, “green”, “blue”";
-    var q5c3 = "var colors = [1:“red”, 2:“green”, 3:“blue”]";
-    var q5c4 = "var colors = (“red”, “green”, “blue”)";
-  
-    // Question six content
-    var questionSix = "Which one of these Disneyland rides is NOT based on a movie?";
-    var q6c1 = "Mr. Toads Wild Ride";
-    var q6c2 = "Pinocchio's Daring Adventure";
-    var q6c3 = "Pirates of the Caribbean";
-    var q6c4 = "Don't be tricky - they are all based on movies";
+// Timer for quiz
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function () {
+  seconds--;
+  document.getElementById("countdown").textContent = seconds;
+  if (seconds <= 0) {
+    clearInterval(countdown);
+    alert("TIMES UP");
+    window.location = "try-again.html";
+  }
+}, 1000);
 
-    // Setting text content to match all question and answers for question 1
-    $(".questionText").text(questionOne);
-    $("#choice1").text(q1c1);
-    $("#choice2").text(q1c2);
-    $("#choice3").text(q1c3);
-    $("#choice4").text(q1c4);
-  
-    // On-click functions for each answer - moving user to next question
-    $("#choice1").on("click", function() {
-        alert("Correct!");
-        answerCorrect++;
-        nextQuestion2 ();
-    });
-    $("#choice2").on("click", function() {
-        alert("Not Quite!");
-        seconds -= 10;
-        nextQuestion2 ();
-    });
-    $("#choice3").on("click", function() {
-        alert("Not Quite!");
-        seconds -= 10;
-        nextQuestion2 ();
-    });
-    $("#choice4").on("click", function() {
-        alert("Not Quite!");
-        seconds -= 10;
-        nextQuestion2 ();
-    });
-   
-    // Changing text for next question - unbinding click handlers to match new answers
-    function nextQuestion2 () {
-        $("#choice1").text(q2c1).unbind();
-        $("#choice2").text(q2c2).unbind();
-        $("#choice3").text(q2c3).unbind();
-        $("#choice4").text(q2c4).unbind();
-        $(".questionText").text(questionTwo);
-        changeAnswer2 ();
-     };
-   
-     // Question 2 updated click handlers
-     function changeAnswer2 () {
-        $("#choice1").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion3 ()
-        });
-        $("#choice2").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion3 ()
-        });
-        $("#choice3").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion3 ()
-        });
-        $("#choice4").on("click", function() {
-            alert("Correct!");
-            answerCorrect++;
-            nextQuestion3 ();
-        });
-     };
-   
-     // Changing text for next question - unbinding click handlers to match new answers
-     function nextQuestion3 () {
-        $("#choice1").text(q3c1).unbind();
-        $("#choice2").text(q3c2).unbind();
-        $("#choice3").text(q3c3).unbind();
-        $("#choice4").text(q3c4).unbind();
-        $(".questionText").text(questionThree);
-        changeAnswer3 ();
-     };
-   
-     // Question 3 updated click handlers
-     function changeAnswer3 () {
-        $("#choice1").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion4 ();
-        });
-        $("#choice2").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion4 ();
+//Setting variable selectors
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
+var questionText = document.querySelector(".questionText");
+var answerCorrect = 0;
 
-        });
-        $("#choice3").on("click", function() {
-            alert("Not Quite!");
-            seconds -= 10;
-            nextQuestion4 ();
+// Question one content
+var questionOne = "Inside which HTML element do we put the JavaScript?";
+var q1c1 = "<script>";
+var q1c2 = "<scripting>";
+var q1c3 = "<javascript>";
+var q1c4 = "<js>";
 
-        });
-        $("#choice4").on("click", function() {
-            alert("Correct!");
-            answerCorrect++;
-            nextQuestion4 ();
-            
-        });
-    };
-   
-    // Changing text for next question - unbinding click handlers to match new answers
-     function nextQuestion4 () {
-        $("#choice1").text(q4c1).unbind();
-        $("#choice2").text(q4c2).unbind();
-        $("#choice3").text(q4c3).unbind();
-        $("#choice4").text(q4c4).unbind();
-        $(".questionText").text(questionFour);
-        changeAnswer4 ();
-        };
-        // Question 4 updated click handlers
-     function changeAnswer4 () {
-            $("#choice1").on("click", function() {
-                alert("Correct!");
-                answerCorrect++;
-                nextQuestion5 ()
-            });
-            $("#choice2").on("click", function() {
-                alert("Not Quite!");
-                seconds -= 10;
-                nextQuestion5 ()
-    
-            });
-            $("#choice3").on("click", function() {
-                alert("Not Quite!");
-                seconds -= 10;
-                nextQuestion5 ()
-    
-            });
-            $("#choice4").on("click", function() {
-                alert("Not Quite!");
-                seconds -= 10;
-                nextQuestion5 ()
-                
-            });
-        };
-       
-        // Changing text for next question - unbinding click handlers to match new answers
-     function nextQuestion5 () {
-            $("#choice1").text(q5c1).unbind();
-            $("#choice2").text(q5c2).unbind();
-            $("#choice3").text(q5c3).unbind();
-            $("#choice4").text(q5c4).unbind();
-            $(".questionText").text(questionFive);
-            changeAnswer5 ();
-            };
-         
-            // Question 5 updated click handlers
-     function changeAnswer5 () {
-                $("#choice1").on("click", function() {
-                    alert("Correct!");
-                    answerCorrect++;
-                    nextQuestion6 ()
-                });
-                $("#choice2").on("click", function() {
-                    alert("Not Quite!");
-                    seconds -= 10;
-                    nextQuestion6 ()
-        
-                });
-                $("#choice3").on("click", function() {
-                    alert("Not Quite!");
-                    seconds -= 10;
-                    nextQuestion6 ()
-        
-                });
-                $("#choice4").on("click", function() {
-                    alert("Not Quite!");
-                    seconds -= 10;
-                    nextQuestion6 ()
-                    
-                });
-            };
-         
-            // Changing text for next question - unbinding click handlers to match new answers    
-     function nextQuestion6 () {
-                $("#choice1").text(q6c1).unbind();
-                $("#choice2").text(q6c2).unbind();
-                $("#choice3").text(q6c3).unbind();
-                $("#choice4").text(q6c4).unbind();
-                $(".questionText").text(questionSix);
-                changeAnswer6 ();
-                };
-            
-                //Question 6 updated click handlers
-     function changeAnswer6 () {
-                    $("#choice1").on("click", function() {
-                        alert("Not Quite!");
-                        seconds -= 10;
-                        if (seconds <=0) { window.location.href="try-again.html" } else { highScore () };
-                    });
-                    $("#choice2").on("click", function() {
-                        alert("Not Quite!");
-                        seconds -= 10;
-                        if (seconds <=0) { window.location.href="try-again.html" } else { highScore () };
-                    });
-                    $("#choice3").on("click", function() {
-                        alert("Correct!");
-                        answerCorrect++;
-                        if (seconds <=0) { window.location.href="try-again.html" } else { highScore () };
-                    });
-                    $("#choice4").on("click", function() {
-                        alert("Not Quite!");
-                        seconds -= 10;
-                        if (seconds <=0) { window.location.href="try-again.html" } else { highScore () };
-                    });
-                };
-        // Function called to show users score and time remaining        
-        function highScore () {
-        $("#timeLeft").empty();
-        $("button").empty();
-        $(".questionAnswers").empty();
-        $(".questionText").text("You answered " + answerCorrect + " questions correct!");
-        $(".questionText").addClass("title container");
-        $(".questionText").css({ "font-size" : "60px", "text-align" : "center" });
-        $("<div></div>").appendTo("h2").css({ "color" : "green", "margin-top" : "100px"}).text("Total time remaining " + seconds + " seconds")
-        $("div").removeClass("questionAnswers")
-        $("<a></a>").appendTo(".highScore").addClass("btn btn-primary btn-lg btn-block btn-hover").text("Enter your score").attr("href", "high-score.html")
-        
-        
-       
-    };
+// Question two content
+var questionTwo = "How do you write ''Hello World'' in an alert box?";
+var q2c1 = "msgBox(''Hello World'');";
+var q2c2 = "msg(''Hello World'');";
+var q2c3 = "alertBox(''Hello World'');";
+var q2c4 = "alert(''Hello World'');";
 
-       
-    
+// Question three content
+var questionThree = "How do you write an IF statement in JavaScript";
+var q3c1 = "if i = 5";
+var q3c2 = "if i == 5 then";
+var q3c3 = "if i = 5 then";
+var q3c4 = "if (i == 5)";
 
-            
-    
-    
-    
+// Question four content
+var questionFour = "How does a FOR loop start?";
+var q4c1 = "for (i = 0; i < 5; i++)";
+var q4c2 = "for (i = 0; i < 5)";
+var q4c3 = "for (i < 5; i++)";
+var q4c4 = "for (i =1 to 5)";
 
+// Question five content
+var questionFive = "What is the correct way to write a JavaScript array?";
+var q5c1 = "var colors = [“red”, “green”, “blue”]";
+var q5c2 = "var colors = “red”, “green”, “blue”";
+var q5c3 = "var colors = [1:“red”, 2:“green”, 3:“blue”]";
+var q5c4 = "var colors = (“red”, “green”, “blue”)";
 
+// Question six content
+var questionSix = "Which one of these Disneyland rides is NOT based on a movie?";
+var q6c1 = "Mr. Toads Wild Ride";
+var q6c2 = "Pinocchio's Daring Adventure";
+var q6c3 = "Pirates of the Caribbean";
+var q6c4 = "Don't be tricky - they are all based on movies";
 
+// Function to show question
+function displayQuestion(q, c1, c2, c3, c4) {
+  document.querySelector("main").innerHTML = `<h2 class="questionText">${q}</h2>
+  <div class="title"></div>
+  <!-- Buttons for user answers-->
+  <div class="questionAnswers highScore">
+    <button type="button" class="btn btn-primary btn-lg" id="choice1">${c1}</button>
+  </div>
+  <div class="questionAnswers">
+    <button type="button" class="btn btn-primary btn-lg" id="choice2">${c2}</button>
+  </div>
+  <div class="questionAnswers">
+    <button
+      type="button"
+      class="btn btn-primary btn-lg special"
+      id="choice3"
+    >${c3}</button>
+  </div>
+  <div class="questionAnswers">
+    <button type="button" class="btn btn-primary btn-lg" id="choice4">${c4}</button>
+  </div>`;
+}
+// Function for correct answer display
+function correctAnswerHandler() {
+  alert("correct");
+}
 
+// Function for incorrect answer display
+function incorrectAnswerHandler() {
+  alert("Not Quite!");
+  seconds -= 10;
+}
 
+// Setting text content to match all question and answers for question 1
+$("#choice1").text(q1c1);
+$("#choice2").text(q1c2);
+$("#choice3").text(q1c3);
+$("#choice4").text(q1c4);
+$(".questionText").text(questionOne);
 
+// On-click functions for each answer - moving user to next question
+$("#choice1").on("click", function () {
+  correctAnswerHandler();
+  nextQuestion2();
+});
+$("#choice2").on("click", function () {
+  incorrectAnswerHandler();
+  nextQuestion2();
+});
+$("#choice3").on("click", function () {
+  incorrectAnswerHandler();
+  nextQuestion2();
+});
+$("#choice4").on("click", function () {
+  incorrectAnswerHandler();
+  nextQuestion2();
+});
 
+// Changing text for next question
+function nextQuestion2() {
+  displayQuestion(questionTwo, q2c1, q2c2, q2c3, q2c4);
+  changeAnswer2();
+}
 
+// Question 2 updated click handlers
+function changeAnswer2() {
+  $("#choice1").on("click", function () {
+    incorrectAnswerHandler();
 
+    nextQuestion3();
+  });
+  $("#choice2").on("click", function () {
+    incorrectAnswerHandler();
 
+    nextQuestion3();
+  });
+  $("#choice3").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion3();
+  });
+  $("#choice4").on("click", function () {
+    correctAnswerHandler();
+
+    nextQuestion3();
+  });
+}
+
+// Changing text for next question
+function nextQuestion3() {
+  displayQuestion(questionThree, q3c1, q3c2, q3c3, q3c4);
+  changeAnswer3();
+}
+
+// Question 3 updated click handlers
+function changeAnswer3() {
+  $("#choice1").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion4();
+  });
+  $("#choice2").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion4();
+  });
+  $("#choice3").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion4();
+  });
+  $("#choice4").on("click", function () {
+    correctAnswerHandler();
+
+    nextQuestion4();
+  });
+}
+
+// Changing text for next question
+function nextQuestion4() {
+  displayQuestion(questionFour, q4c1, q4c2, q4c3, q4c4);
+  changeAnswer4();
+}
+// Question 4 updated click handlers
+function changeAnswer4() {
+  $("#choice1").on("click", function () {
+    correctAnswerHandler();
+
+    nextQuestion5();
+  });
+  $("#choice2").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion5();
+  });
+  $("#choice3").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion5();
+  });
+  $("#choice4").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion5();
+  });
+}
+
+// Changing text for next question
+function nextQuestion5() {
+  displayQuestion(questionFive, q5c1, q5c2, q5c3, q5c4);
+  changeAnswer5();
+}
+
+// Question 5 updated click handlers
+function changeAnswer5() {
+  $("#choice1").on("click", function () {
+    correctAnswerHandler();
+
+    nextQuestion6();
+  });
+  $("#choice2").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion6();
+  });
+  $("#choice3").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion6();
+  });
+  $("#choice4").on("click", function () {
+    incorrectAnswerHandler();
+
+    nextQuestion6();
+  });
+}
+
+// Changing text for next question
+function nextQuestion6() {
+  displayQuestion(questionSix, q6c1, q6c2, q6c3, q6c4);
+  changeAnswer6();
+}
+
+//Question 6 updated click handlers
+function changeAnswer6() {
+  $("#choice1").on("click", function () {
+    incorrectAnswerHandler();
+
+    if (seconds <= 0) {
+      window.location.href = "try-again.html";
+    } else {
+      highScore();
+    }
+  });
+  $("#choice2").on("click", function () {
+    incorrectAnswerHandler();
+
+    if (seconds <= 0) {
+      window.location.href = "try-again.html";
+    } else {
+      highScore();
+    }
+  });
+  $("#choice3").on("click", function () {
+    correctAnswerHandler();
+
+    if (seconds <= 0) {
+      window.location.href = "try-again.html";
+    } else {
+      highScore();
+    }
+  });
+  $("#choice4").on("click", function () {
+    incorrectAnswerHandler();
+
+    if (seconds <= 0) {
+      window.location.href = "try-again.html";
+    } else {
+      highScore();
+    }
+  });
+}
+// Function called to show users score and time remaining
+function highScore() {
+  $("#timeLeft").empty();
+  $("button").empty();
+  $(".questionAnswers").empty();
+  $(".questionText").text("You answered " + answerCorrect + " questions correct!");
+  $(".questionText").addClass("title container");
+  $(".questionText").css({ "font-size": "60px", "text-align": "center" });
+  $("<div></div>")
+    .appendTo("h2")
+    .css({ color: "green", "margin-top": "100px" })
+    .text("Total time remaining " + seconds + " seconds");
+  $("div").removeClass("questionAnswers");
+  // Creates button link for user to click to take them to their highscore page
+  $("<a></a>")
+    .appendTo(".highScore")
+    .addClass("btn btn-primary btn-lg btn-block btn-hover")
+    .text("Enter your score")
+    .attr("href", "high-score.html");
+}
 
 //DID NOT USE, KEPT FOR LEARNING AND TO GO OVER WITH TUTOR
 
-    // Array of question objects
+// Array of question objects
 // var questions = [
 //     {
 //         question: "Inside which HTML element do we put the JavaScript?",
@@ -403,15 +410,3 @@
 //     choice4: "Don't be tricky - they are all based on movies",
 //     answer: 3,
 // };
-
-
-
-
-
-
-
-
-
-
-
-
